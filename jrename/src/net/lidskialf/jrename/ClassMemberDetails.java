70f9596879aab7a66864b938e15aa6934e47ef0a
adqadq <adqadq@gmail.com>
2010-09-28 23:23:57 +0000

    Initial checkin

---
+++ jrename/src/net/lidskialf/jrename/ClassMemberDetails.java
@@ -0,0 +1,11 @@
+package net.lidskialf.jrename;
+
+public class ClassMemberDetails {
+	public String oldName;
+	public String newName;
+	
+	public ClassMemberDetails(String oldName, String newName) {
+		this.oldName = oldName;
+		this.newName = newName;
+	}
+}
