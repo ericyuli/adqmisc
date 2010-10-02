65d07ac53f0f1bef981183a1e97cd5b097372bfc
adqadq <adqadq@gmail.com>
2010-10-02 02:23:10 +0000

    Attempt to preserve names of inherited members. Methods with the same names+return types in local classes are all scrambled anyway, so don't bother trying to keep them with the same names any more

--- jrename/src/net/lidskialf/jrename/ClassMemberDetails.java
+++ jrename/src/net/lidskialf/jrename/ClassMemberDetails.java
@@ -2,18 +2,16 @@ package net.lidskialf.jrename;
 
 public class ClassMemberDetails {
 	public String name;
-	public String returnDesc;
-	public String argsDesc;
+	public String desc;
 	public Object value;
 
-	public ClassMemberDetails(String oldName, String typeDesc, Object value) { /* for a field */
-		this(oldName, typeDesc, null);
+	public ClassMemberDetails(String oldName, String desc, Object value) { /* for a field */
+		this(oldName, desc);
 		this.value = value;
 	}
 
-	public ClassMemberDetails(String oldName, String returnDesc, String argsDesc) { /* for a method */
+	public ClassMemberDetails(String oldName, String desc) { /* for a method */
 		this.name = oldName;
-		this.returnDesc = returnDesc;
-		this.argsDesc = argsDesc;
+		this.desc = desc;
 	}
 }
