1411fde6c58f3728fd7efc4193cf7decc25abe8d
adqadq@gmail.com <adqadq@gmail.com@6b8980de-2ae2-11de-94d6-134fa40061eb>
2010-10-08 23:31:35 +0000

    Implement file loading APIs

--- trunk/kif/kif/net/lidskialf/kif/FileSelectorPanel.java
+++ trunk/kif/kif/net/lidskialf/kif/FileSelectorPanel.java
@@ -3,6 +3,7 @@ package net.lidskialf.kif;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 
 import org.kwt.ui.KWTSelectableLabel;
@@ -10,25 +11,21 @@ import org.kwt.ui.KWTSelectableLabel;
 import com.amazon.kindle.kindlet.ui.KBoxLayout;
 import com.amazon.kindle.kindlet.ui.KPanel;
 
-public class GameSelectorPanel extends KPanel {
+public class FileSelectorPanel extends KPanel {
 
 	private static final long serialVersionUID = 8134629461211328457L;
 	
-	private KifKindlet kindlet;
+	public FileSelectorPanel(final KifKindlet kindlet, final String selectionType, final File basePath, FilenameFilter filter) {
 	
-	public GameSelectorPanel(final KifKindlet kindlet) {
-		
-		this.kindlet = kindlet;
-
 		this.setLayout(new KBoxLayout(this, KBoxLayout.Y_AXIS));
 		
         ActionListener gameSelectedAction = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				kindlet.loadGame(((KWTSelectableLabel) e.getSource()).getText());
+				kindlet.fileSelected(selectionType, new File(basePath, ((KWTSelectableLabel) e.getSource()).getText()));
 			}
 		};
 		
-		File[] games = getGames();
+		File[] games = getFiles(basePath, filter);
         for (int i = 0; i < games.length; i++) {
             KWTSelectableLabel label = new KWTSelectableLabel(games[i].getName());
             label.setFocusable(true);
@@ -39,29 +36,10 @@ public class GameSelectorPanel extends KPanel {
         }        
 	}
 	
-	private File[] getGames() {
-		ArrayList results = new ArrayList();
-		File homeDir = kindlet.getContext().getHomeDirectory();
-		if (!homeDir.exists())
+	private File[] getFiles(File basePath, FilenameFilter filter) {
+		if (!basePath.exists())
 			return new File[0];
 		
-		File[] files = homeDir.listFiles();
-		for(int i=0; i< files.length; i++) {
-			if (files[i].isDirectory())
-				continue;
-			
-			String filename = files[i].getName();
-			int dotPos = filename.indexOf('.');
-			if (dotPos == -1)
-				continue;
-			
-			String ext = filename.substring(dotPos+1).toLowerCase();
-			if (ext.charAt(0) != 'z')
-				continue;
-			
-			results.add(files[i]);
-		}
-		
-		return (File[]) results.toArray(new File[results.size()]);
+		return basePath.listFiles(filter);
 	}
 }
