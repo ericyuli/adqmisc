1c357f16c00e94ecf0cee2d93aa79a3311e1de1b
adqadq <adqadq@gmail.com>
2010-10-24 18:16:13 +0000

    Include licensing information

--- kif/kif/net/lidskialf/kif/KifKindlet.java
+++ kif/kif/net/lidskialf/kif/KifKindlet.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010 Andrew de Quincey.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.lidskialf.kif;
 
 import java.awt.BorderLayout;
@@ -511,12 +527,13 @@ public class KifKindlet implements Kindlet, StatusLine, StatusLineListener,
 	private Component createAboutComponent() {
 		KLabelMultiline label = new KLabelMultiline(
 				"Kif - an Infocom interpreter for the Kindle!\n"
-						+ "(c) 2010 Andrew de Quincey\n\n"
-						+ "Includes ZMPP, (c) 2005-2010, Wei-ju Wu (GPL).\n"
-						+ "Includes KWT, (c) 2010 Dan Fabulich (Apache license).\n\n"
+						+ "(c) 2010 Andrew de Quincey, released under the Apache 2.0 license.\n\n"
+						+ "Includes ZMPP, (c) 2005-2010, Wei-ju Wu, Box of Rats LLC, All rights reserved.\n"
+						+ "Includes KWT, (c) 2010 Dan Fabulich (Apache 2.0 licensed).\n\n"
 						+ "Press Menu to open a game.");
 		return label;
 	}
+
 
 	private void initRootContent() {
 		root.add(KBox.createHorizontalStrut(inset), BorderLayout.EAST);
