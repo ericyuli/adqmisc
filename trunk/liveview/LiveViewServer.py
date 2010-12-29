1baa3c7d985642292fd1e2ca160e199172ec0ff2
adqadq@gmail.com <adqadq@gmail.com@6b8980de-2ae2-11de-94d6-134fa40061eb>
2010-12-29 17:24:52 +0000

    Figure out some more of the protocol

--- trunk/liveview/LiveViewServer.py
+++ trunk/liveview/LiveViewServer.py
@@ -12,24 +12,27 @@ bluetooth.advertise_service(serverSocket, "LiveView",
 			    service_classes=[ bluetooth.SERIAL_PORT_CLASS ],
 			    profiles=[ bluetooth.SERIAL_PORT_PROFILE ]			    
 			    )
-
-
 clientSocket, address = serverSocket.accept()
 
-clientSocket.send(LiveViewProtocol.EncodeCapsReq())
+clientSocket.send(LiveViewProtocol.EncodeGetCaps())
 deviceCaps = LiveViewProtocol.Decode(clientSocket.recv(1024))
+clientSocket.send(LiveViewProtocol.EncodeAck(LiveViewProtocol.LV_MSG_GETCAPS))
+print deviceCaps
 
-#clientSocket.send(LiveViewProtocol.EncodeClearDisplayReq())
-#print LiveViewProtocol.Decode(clientSocket.recv(1024))
+clientSocket.send(LiveViewProtocol.EncodeSetMenuSize(5))
+clientSocket.send(LiveViewProtocol.EncodeSetSettings(0, 12, 4))
 
-#clientSocket.send(LiveViewProtocol.EncodeStandbyReq(2))
-#print LiveViewProtocol.Decode(clientSocket.recv(1024))
-
-#clientSocket.send(LiveViewProtocol.EncodeLEDReq(0, 0, 0x1f, 10, 1000))
-#print LiveViewProtocol.Decode(clientSocket.recv(1024))
-
-#clientSocket.send(LiveViewProtocol.EncodeVibrateReq(10, 1000))
-#print LiveViewProtocol.Decode(clientSocket.recv(1024))
+while True:
+	tmp = LiveViewProtocol.Decode(clientSocket.recv(1024))
+	if isinstance(tmp, LiveViewProtocol.GetMenuItems):
+		print tmp
+	elif isinstance(tmp, LiveViewProtocol.DisplayCapabilities):
+		print tmp
+	elif isinstance(tmp, LiveViewProtocol.Result):
+		print tmp
+	else:
+		print "UNKNOWN"
+		print tmp
 
 clientSocket.close()
 serverSocket.close()
